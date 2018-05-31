cf8b97638653e8d87b81e5fb3b5ba516b49f77fa
marten-lucas <35145595+marten-lucas@users.noreply.github.com>
2018-05-31 15:57:33 +0200

    Button in SVG

--- script/foosball_table_animate.js
+++ script/foosball_table_animate.js
@@ -4,6 +4,10 @@ function move_rod(id,dir){
 	alert("jetze");
 }
 
+function guides_off(){
+	document.getElementById("guides").setAttribute("visibility","hidden");
+}
+
 function toggle_guides() {
 	var checkbox_guides = document.getElementById("checkbox_toggle_guides");
     
