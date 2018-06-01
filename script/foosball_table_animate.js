9156bbe74f7ca4a4d582b7aede77735b78251784
marten-lucas <35145595+marten-lucas@users.noreply.github.com>
2018-06-01 16:07:43 +0200

    tried limitation

--- script/foosball_table_animate.js
+++ script/foosball_table_animate.js
@@ -22,9 +22,10 @@ function toggle_guides() {
 
 
 function rod_move(evt) {
-	var step_size = 10
+	var step_size = 5
 	var rod_name = evt.target.id.substring(0, 4);
 	var rod_selector = "#"+rod_name+"_up,#"+rod_name+"_front,#"+rod_name+"_back";
+	var arrow_clicked = s.select("#"+evt.target.id);
 	
 
 	
@@ -42,24 +43,37 @@ function rod_move(evt) {
         break;
     };
 
-
+	var table = s.select("#table");
+	var bb_table = table.getBBox();
 	
 	$(rod_selector).each(function(i, rod) {
 		
 		var rod_svg = s.select("#"+rod.id);
-		
 		var matrix = rod_svg.transform().localMatrix
-		
+				
 		matrix.f = matrix.f + step_size * direction
-		
 		rod_svg.transform(matrix);
+	
+		//check if further step is possible
+		bb_rod = rod_svg.getBBox();
 		
+		// if (rod_svg.attr("display")=="inline" & bb_rod.Y2 + step_size * direction < bb_table.Y2 && bb_rod.Y + step_size * direction >bb_table.Y) {
+		// arrow_clicked.attr({ display : "inline" });   	
+		// } else {
+		// arrow_clicked.attr({ display : "none" });   	
+		// }
+		
+		
+	});	
+	
+	if (/toward/.test(evt.target.id)) {
+	var arrow_opposite = s.select("#" + rod_name + "_move_away");
+	} else {
+	var arrow_opposite = s.select("#" + rod_name + "_move_toward");
+	};
+	arrow_opposite.attr({ display : "inline" });   
 
-	});
-	
-	
 
-	
 };
 
 
